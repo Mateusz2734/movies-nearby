09c1bd6e6d09ff003e38a35258932fe38129f7ac
Mateusz Wala <mateusz.wala2734@gmail.com>
2022-08-22 02:59:27 +0200

    Update Helios scraper

--- src/index.ts
+++ src/index.ts
@@ -7,12 +7,12 @@ const PORT = 4000;
 const app = express();
 
 app.get('/starowka', async (req: Request, res: Response) => {
-  const result = await naStarowce("2022-08-17");
+  const result = await naStarowce("2022-09-20");
   res.json(result);
 });
 
 app.get('/helios', async (req: Request, res: Response) => {
-  const result = await helios("2022-08-17");
+  const result = await helios("2022-07-30");
   res.json(result);
 });
 
--- src/scrapers/helios.ts
+++ src/scrapers/helios.ts
@@ -1,17 +1,34 @@
 import { split, trim, zip, slice } from "lodash";
 import { load } from "cheerio";
+import { howManyDaysFromToday } from "../utils/dates.util";
 import axios from "axios";
 import dayjs from 'dayjs';
 
 export default async function run(date: string) {
-  const result = await axios.get("https://www.helios.pl/64,Zory/Repertuar/index/dzien/0/kino/64");
-  const $ = load(result.data);
-  const titles = slice(split($(".movie-link").text(), "\n").map(info => trim(info)), 1);
-  const infos = split($("div.time").text(), "\n").map(info => info.replace(/\t/g, '')).map(info => info.replace(/\*/g, '').match(/.{1,5}/g)).slice(-titles.length);
-  const zipped = zip(titles, infos);
-  const movies: Object[] = [];
-  for (const entry of zipped) {
-    movies.push({ title: entry[0], time: entry[1] });
+  const urlDay = howManyDaysFromToday(date);
+  if (urlDay < 0 || urlDay > 7) {
+    // Return unified object with empty array to prevent axios errors
+    return { date: dayjs(date).format("YYYY-MM-DD"), cinema: "Kino Helios Żory", movies: [] };
+  } else {
+    const result = await axios.get(`https://www.helios.pl/64,Zory/Repertuar/index/dzien/${urlDay}`);
+    const $ = load(result.data);
+    // Parse the titles according to the page
+    const titles = slice(split($(".movie-link").text(), "\n").map(info => trim(info)), 1);
+    // Parse the times according to the page
+    const infos = split($("div.time").text(), "\n").map(info => info.replace(/\t/g, '')).map(info => info.replace(/\*/g, '').match(/.{1,5}/g)).slice(-titles.length);
+    if (infos.includes(null)) {
+      // Return unified object with empty array to prevent weird behavior
+      return { date: dayjs(date).format("YYYY-MM-DD"), cinema: "Kino Helios Żory", movies: [] };
+    } else {
+      // Connect info and title array
+      const zipped = zip(titles, infos);
+      // New array for proper output format
+      const movies: Object[] = [];
+      for (const entry of zipped) {
+        movies.push({ title: entry[0], time: entry[1] });
+      }
+      // Return unified object
+      return { date: dayjs(date).format("YYYY-MM-DD"), cinema: "Kino Helios Żory", movies: movies };
+    }
   }
-  return { date: dayjs(date).format("YYYY-MM-DD"), cinema: "Kino Helios Żory", movies: movies };
 }
