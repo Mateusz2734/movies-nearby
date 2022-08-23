fc58120c402b8a387e9393391cade47307d88424
Mateusz Wala <mateusz.wala2734@gmail.com>
2022-08-23 20:29:29 +0200

    Add geocoding API support

--- src/config/default.ts
+++ src/config/default.ts
@@ -3,5 +3,6 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export default {
-  db_url: process.env.DB_URL as string
+  db_url: process.env.DB_URL as string,
+  geo_api_key: process.env.GEO_API_KEY as string
 };
---
+++ src/service/geocoding.service.ts
@@ -0,0 +1,18 @@
+import axios from 'axios';
+import config from '../config/default';
+
+export interface geocodingResponse {
+  city: string,
+  lon: number,
+  lat: number;
+}
+
+export default async function encodeCity(city: string): Promise<geocodingResponse> {
+  // Encode the city name to put in API Request
+  const urlEncodedCity: string = encodeURI(city);
+  // Send request to Geocoding API
+  const apiResponse = await axios.get(`http://api.openweathermap.org/geo/1.0/direct?appid=${config.geo_api_key}&q=${urlEncodedCity}&limit=1`);
+  // Get longitude and latitude
+  const { lon, lat } = apiResponse.data[0];
+  return { city: city, lon: lon, lat: lat };
+}
