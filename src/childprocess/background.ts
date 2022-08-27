f0fa5cf01fe4e81267f0f6dc9ddcfc17769dc32a
Mateusz Wala <mateusz.wala2734@gmail.com>
2022-08-27 23:15:02 +0200

    Add new date functions

--- src/childprocess/background.ts
+++ src/childprocess/background.ts
@@ -2,11 +2,12 @@ import cinemaList from "../db/cinemaList.db";
 import { chooseScraperAndExecute } from "../controllers/scrapers.controller";
 import { createResult, findResultWithSpecificDateAndCinema } from "../services/db.service";
 import { connectWithDatabase } from "../db/connect";
+import { todayAndFiveNextDays } from "../utils/dates.util";
 
 export async function backgroundScraping() {
-  for (const cinema of cinemaList) {
-    const dates = ["2022-08-27", "2022-08-28"];
-    for (const date of dates) {
+  const dates: string[] = todayAndFiveNextDays();
+  for (const date of dates) {
+    for (const cinema of cinemaList) {
       try {
         const foundResult = await findResultWithSpecificDateAndCinema({ date: date, ...cinema });
         if (!foundResult) {
--- src/utils/dates.util.ts
+++ src/utils/dates.util.ts
@@ -1,3 +1,5 @@
+import dayjs from "dayjs";
+
 export function howManyDaysFromToday(day: string | Date): number {
   // Works good in GMT+2 timezone
   const todayDate: Date = new Date(new Date().setHours(2, 0, 0, 0));
@@ -9,4 +11,14 @@ export function howManyDaysFromToday(day: string | Date): number {
   // Number of days between two dates
   let differenceInDays: number = Math.round(diffInTime / ONE_DAY);
   return differenceInDays;
+}
+
+export function xthDay(x: number): string {
+  let tomorrow: Date = new Date();
+  return dayjs(tomorrow.setDate(tomorrow.getDate() + x)).format("YYYY-MM-DD");
+}
+
+export function todayAndFiveNextDays(): string[] {
+  const date: string = dayjs(new Date()).format("YYYY-MM-DD");
+  return [xthDay(0), xthDay(1), xthDay(2), xthDay(3), xthDay(4), xthDay(5)];
 }
