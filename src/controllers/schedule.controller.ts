68614c5e4b885e2cb4297a166099be831c939620
Mateusz Wala <mateusz.wala2734@gmail.com>
2022-09-12 00:22:03 +0200

    fix: change unexpected behaviour after midnight

--- src/controllers/schedule.controller.ts
+++ src/controllers/schedule.controller.ts
@@ -2,6 +2,7 @@ import { scheduleJob, RecurrenceRule } from "node-schedule";
 import { startBackgroundScraping } from "../services/background.service";
 
 const scrapingRule = new RecurrenceRule();
+scrapingRule.minute = 0;
 scrapingRule.hour = 0;
 scrapingRule.tz = "Europe/Warsaw";
 
